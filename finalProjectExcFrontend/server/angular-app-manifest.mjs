9a79a22188414fdc6315bde3749593c72ddba5fc
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-11-21 22:08:42 +0100

    updated

--- finalProjectExcFrontend/server/angular-app-manifest.mjs
+++ finalProjectExcFrontend/server/angular-app-manifest.mjs
@@ -44,10 +44,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 26736, hash: '86137ffd93d83e1cd9774dc046b27a9f628d0626e12bad36b2a79d8d42f2fe70', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 19151, hash: '50bcfcfb706f7face363ab3faec1c8b796f59dce1e40ea558ac0dec1c0084431', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'error500/index.html': {size: 29553, hash: '7753395f4ea741923c0e6b2c1b1f6b4d9540e29f5d38069f5ab3fb23815d7bf2', text: () => import('./assets-chunks/error500_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 106131, hash: '8662b5d6842b09e07515e2544fb251c9f32d932e6b0739b54bc92281e454f207', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 26739, hash: '122755eafd172673a3c96b02c4a817bf55bd3cec34bec7bce8c5819c63e352a9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 19154, hash: 'c33146d5dd32c95e9f1a15fbbb4c9eeddd35c785393f1857f76643e16ae6d1c4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'error500/index.html': {size: 29556, hash: '638a97851142ca4b5881c89142df8ec74f63d407b9e80b0318bff0db99e4117f', text: () => import('./assets-chunks/error500_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 131377, hash: '504390792ece864d541ddf7ae1576e81527b70cc009df855945be8ed65baeb35', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-OPZ5M2CM.css': {size: 8141, hash: 'qBtxzILT0lE', text: () => import('./assets-chunks/styles-OPZ5M2CM_css.mjs').then(m => m.default)}
   },
 };
--- finalProjectExcFrontend/server/assets-chunks/index_server_html.mjs
+++ finalProjectExcFrontend/server/assets-chunks/index_server_html.mjs
@@ -2,7 +2,7 @@ export default `<!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>FinalProjectExcFrontend</title>
+  <title>Angular FullStack ToDoList</title>
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
@@ -18,6 +18,6 @@ export default `<!doctype html>
 <body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
 </script>
   <app-root></app-root>
-<link rel="modulepreload" href="chunk-R7DQZ7LP.js"><link rel="modulepreload" href="chunk-ILANDEX4.js"><link rel="modulepreload" href="chunk-QIXNOVUR.js"><script src="polyfills-5CFQRCPP.js" type="module"></script><script src="main-CXT2LCTV.js" type="module"></script></body>
+<link rel="modulepreload" href="chunk-R7DQZ7LP.js"><link rel="modulepreload" href="chunk-ILANDEX4.js"><link rel="modulepreload" href="chunk-QIXNOVUR.js"><script src="polyfills-5CFQRCPP.js" type="module"></script><script src="main-ZKEDLF36.js" type="module"></script></body>
 </html>
 `;
